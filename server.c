b0c030b19ceb2369f97961328537a782bd8efbc6
lior666666 <75208314+lior666666@users.noreply.github.com>
2021-12-30 21:09:20 +0000

    next version - part 1 unfinished

--- server.c
+++ server.c
@@ -1,6 +1,5 @@
 #include "segel.h"
 #include "request.h"
-#include <pthread.h>
 //#include <condition_variable>
 
 // 
@@ -14,72 +13,134 @@
 //
 
 
+
+// A linked list node
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* last = NULL;
+    int size = 0;
+};
+
+/* Given a reference (pointer to pointer) to the head of a
+   list and an int, inserts a new node on the front of the
+   list. */
+void push_back(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    struct Node *temp = *head_ref, *prev;
+    while (temp != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = new_node;
+    new_node->next = NULL;
+    size++;
+}
+
+void deleteNode(struct Node** head_ref, int key)
+{
+    // Store head node
+    struct Node *temp = *head_ref, *prev;
+ 
+    // If head node itself holds the key to be deleted
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // Changed head
+        free(temp); // free old head
+        return;
+    }
+ 
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+ 
+    // Unlink the node from linked list
+    prev->next = temp->next;
+ 
+    free(temp); // Free memory
+    size--;
+}
+
+
+
+
 // We need to check if it's should be here:
 // ALSO NEED TO CHECK HOW TO INITIALIZE THEM
 cond_t c;
+pthread_cond_init(&c, NULL);
 mutex_lock m;
-int working_threads_counter = 0;
-int waiting_threads_counter = 0;
+pthread_mutex_init(&m, NULL);
 
-// HW3: Parse the new arguments too
+struct Node* waiting_requests = NULL;
+struct Node* working_requests = NULL;
+
+
+// HW3: Parse the new arguments to
 void getargs(int *port, int argc, int *thread_number, int *queue_size , char *argv[])
 {
+	//need to change the checking of args number
     if (argc < 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	exit(1);
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
     }
     *port = atoi(argv[1]);
-    *thread_number = atoi(arv[2]);
+    *thread_number = atoi(argv[2]);
     *queue_size = atoi(argv[3]);
 }
 
-void* handle(void* port)
+void* handle(void* listenfd)
 {
 	
-	//struct sockaddr_in clientaddr;
-	struct sockaddr_in clientaddr;
-	listenfd = Open_listenfd(port);
-    cond_t c;
-    while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-	
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	
-	requestHandle(connfd);
 
-	Close(connfd);
+    //cond_t c;
+    while (1) {
+		mutex_lock(&m);
+		while((struct Node* waiting_requests == NULL)
+		{
+			cond_wait(&c, &m);
+		}
+		// 
+		// HW3: In general, don't handle the request in the main thread.
+		// Save the relevant info in a buffer and have one of the worker threads 
+		// do the work. 
+		// 
+		
+		requestHandle(connfd);
+
+		Close(connfd);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen, threads_number, queue_size;
-    
+    int listenfd, port, threads_number, queue_size;
+
     getargs(&port, argc, &threads_number, &queue_size, argv);
-    
+	int connfd, clientlen;
+	struct sockaddr_in clientaddr;
     // 
     // HW3: Create some threads...
     //
     pthread_t threads[threads_number];
-    
-    
     for(unsigned int i =0; i<threads_number; i++)
-    {
-		pthread_create(&threads[i], NULL, handle, (void*) &port);
-		pthread_join(threads[i], NULL);
+	{
+		pthread_create(&threads[i], NULL, handle, (void*) waiting_requests);
+	}
+	listenfd = Open_listenfd(port);
+	while (1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(*(int*)listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+		
 	}
 	
-   
+	pthread_join(threads[0], NULL);
 
 }
-
-
-    
-
-
- 
